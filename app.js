a9c5551431a94539d4d7d9cc3acb54d722c59c35
Joey Cos <joeycosentino24@gmail.com>
2022-04-13 19:14:48 -0400

    added greater functionality to creating html

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // LEFT OFF 9.3.1 INTRODUCTION
 
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -142,7 +142,14 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+
+        console.log('Page created! Check out index.html in this directory to see it!');
+
+        });
     });
     
     
