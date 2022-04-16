2998788e4677b7f4c9ee41d4a5dd860ade05ad6c
Joey Cos <joeycosentino24@gmail.com>
2022-04-15 20:20:59 -0400

    committing files

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-// LEFT OFF 9.5.1 INTRODUCTION
-
 const inquirer = require('inquirer');
 const { writeFile, copyFile } = require('./utils/generate-site')
 const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -141,7 +131,7 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => {
+    .then(portfolioData  => { // projectData
         return generatePage(portfolioData);
     })
     .then(pageHTML => {
@@ -157,31 +147,4 @@ promptUser()
     .catch(err => {
         console.log(err);
     });
-    
-    
-    
-    
-    
-    
-    
-    // .then(portfolioData => {
-    //     const pageHTML = generatePage(portfolioData);
-
-    //     fs.writeFile('./dist/index.html', pageHTML, err => {
-    //       if (err) {
-    //           console.log(err);
-    //           return;
-    //       }
-    //       console.log('Page created! Check out index.html in this directory to see it!');
-
-    //       fs.copyFile('./src/style.css', './dist/style.css', err => {
-    //           if (err) {
-    //               console.log(err);
-    //               return;
-    //           }
-    //           console.log('Style sheet copied successfully!');
-    //       });
-    //     });
-    // });
-    
     
